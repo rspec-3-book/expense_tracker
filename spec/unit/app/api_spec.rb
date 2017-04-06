331524d517347004045e97bff17a62482eaf71f6
Myron Marston <myron.marston@gmail.com>
2017-04-06 14:35:30 -0700

    Second passing unit spec.

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -26,7 +26,16 @@ module ExpenseTracker
           expect(parsed).to include('expense_id' => 417)
         end
 
-        it 'responds with a 200 (OK)'
+        it 'responds with a 200 (OK)' do
+          expense = { 'some' => 'data' }
+
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true, 417, nil))
+
+          post '/expenses', JSON.generate(expense)
+          expect(last_response.status).to eq(200)
+        end
       end
 
       context 'when the expense fails validation' do
