1619c573cf8f5ce5c01c56c10bd7058e63a1b120
Myron Marston <myron.marston@gmail.com>
2017-07-24 08:20:30 +0700

    Got first integration spec to pass

---
+++ spec/integration/app/ledger_spec.rb
@@ -0,0 +1,35 @@
+require_relative '../../../app/ledger'
+require_relative '../../../config/sequel'
+require_relative '../../support/db'
+
+module ExpenseTracker
+  RSpec.describe Ledger, :aggregate_failures do
+    let(:ledger) { Ledger.new }
+    let(:expense) do
+      {
+        'payee'  => 'Starbucks',
+        'amount' => 5.75,
+        'date'   => '2017-06-10'
+      }
+    end
+
+    describe '#record' do
+      # ... contexts go here ...
+
+      context 'with a valid expense' do
+        it 'successfully saves the expense in the DB' do
+          result = ledger.record(expense)
+
+          expect(result).to be_success
+          expect(DB[:expenses].all).to match [a_hash_including(
+            id: result.expense_id,
+            payee: 'Starbucks',
+            amount: 5.75,
+            date: Date.iso8601('2017-06-10')
+          )]
+        end
+      end
+
+    end
+  end
+end
