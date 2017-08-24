879d8e98aad31f9769ca244d1e7b562309a74e21
Myron Marston <myron.marston@gmail.com>
2017-08-24 09:53:47 -0700

    Got first integration spec to pass

--- app/ledger.rb
+++ app/ledger.rb
@@ -3,6 +3,9 @@ module ExpenseTracker
 
   class Ledger
     def record(expense)
+      DB[:expenses].insert(expense)
+      id = DB[:expenses].max(:id)
+      RecordResult.new(true, id, nil)
     end
 
     def expenses_on(date)
