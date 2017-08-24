a37d55908b9b3aebca4baff4ea7f452712da8ba7
Myron Marston <myron.marston@gmail.com>
2017-08-24 09:53:23 -0700

    First passing unit spec.

--- app/api.rb
+++ app/api.rb
@@ -3,8 +3,15 @@ require 'json'
 
 module ExpenseTracker
   class API < Sinatra::Base
+    def initialize(ledger: Ledger.new)
+      @ledger = ledger
+      super()
+    end
+
     post '/expenses' do
-      JSON.generate('expense_id' => 42)
+      expense = JSON.parse(request.body.read)
+      result = @ledger.record(expense)
+      JSON.generate('expense_id' => result.expense_id)
     end
 
     get '/expenses/:date' do
---
+++ spec/unit/app/api_spec.rb
@@ -0,0 +1,43 @@
+require_relative '../../../app/api'
+require 'rack/test'
+
+module ExpenseTracker
+  RecordResult = Struct.new(:success?, :expense_id, :error_message)
+
+  RSpec.describe API do
+    include Rack::Test::Methods
+
+    def app
+      API.new(ledger: ledger)
+    end
+
+    let(:ledger) { instance_double('ExpenseTracker::Ledger') }
+
+    describe 'POST /expenses' do
+      context 'when the expense is successfully recorded' do
+        # ... specs go here ...
+
+        it 'returns the expense id' do
+          expense = { 'some' => 'data' }
+
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true, 417, nil))
+
+          post '/expenses', JSON.generate(expense)
+
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include('expense_id' => 417)
+        end
+
+        it 'responds with a 200 (OK)'
+      end
+
+      context 'when the expense fails validation' do
+        # ... specs go here ...
+        it 'returns an error message'
+        it 'responds with a 422 (Unprocessable entity)'
+      end
+    end
+  end
+end
